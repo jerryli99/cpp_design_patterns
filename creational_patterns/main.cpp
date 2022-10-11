47864f3744aa3178420038812388730699a4e1a6
Jerry Li <jli103@terpmail.umd.edu>
2022-10-11 01:50:00 -0400

    Update main.cpp

--- creational_patterns/main.cpp
+++ creational_patterns/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello there design patterns.\n";
+	std::cout << "Hello there, design patterns. This is the creational pattern section!\n";
 	return 0;
 }
